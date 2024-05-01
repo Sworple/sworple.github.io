45d75a18bcc2080a4bd57f53cc63b6b9f4bfc16b
Sworple <smuggreyboi@hotmail.com>
2024-05-01 15:53:24 -0800

    fixes???

--- bing.js
+++ bing.js
@@ -1,5 +1,5 @@
 
-var iframe =document.createElement('iframe');
+//var iframe =document.createElement('iframe');
 
-document.getElementById("iframeDiv").appendChild(iframe);
-iframe.setAttribute("style","src:'https://bing.com';height:100%;width:100%;");
+//document.getElementById("divFrame").appendChild(iframe);
+//iframe.setAttribute("style","src:'https://bing.com';height:100%;width:100%;");
