d7ea7d92f5868a40dd4666a9934b6730592b20fc
Sworple <145728862+Sworple@users.noreply.github.com>
2024-05-01 13:50:09 -0800

    bing develompents

---
+++ bing.js
@@ -0,0 +1,5 @@
+
+var iframe =document.createElement('iframe');
+
+document.getElementById("iframeDiv").appendChild(iframe);
+iframe.setAttribute("style","src:'https://bing.com';height:100%;width:100%;");
