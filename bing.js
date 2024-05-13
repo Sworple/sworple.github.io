6ee6dba87ef2c5d2b66b4ee465ab20cd1e9eada9
Sworple <145728862+Sworple@users.noreply.github.com>
2024-05-13 13:50:30 -0800

    attemps with codeswing + condensing

--- bing.js
+++
@@ -1,5 +0,0 @@
-
-//var iframe =document.createElement('iframe');
-
-//document.getElementById("divFrame").appendChild(iframe);
-//iframe.setAttribute("style","src:'https://bing.com';height:100%;width:100%;");
