262305afeb9109ecef58e8fbc186b8671b6fa5cf
Sworple <145728862+Sworple@users.noreply.github.com>
2024-08-21 16:21:32 +0000

    melon.js install

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ var spinSpeed = 0.01
 
 function spin(object) {
     setInterval(function() {
-        object.style.transform += "rotate(spinSpeed+deg)";
-        spinSpeed + 0.01;
+        object.style.transform += "rotate(0.1deg)";
     }, 1);
 }
