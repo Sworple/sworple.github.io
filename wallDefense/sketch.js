805d40c7b0e9aebec861bcc5d7b7180c7461447b
Sworple <145728862+Sworple@users.noreply.github.com>
2024-08-22 13:50:09 -0800

    yippee

--- wallDefense/sketch.js
+++ wallDefense/sketch.js
@@ -1,10 +1,32 @@
+let cannonballX = 0;
+
+function  preload(){
+
+}
+
 function setup() {
-  //Creates the canvas using windowHeight and windowWidth
   createCanvas(windowWidth, windowHeight);
-  background(0,0,75);
+  noCursor();
+  frameRate(60);
+
 }
 
 function draw() {
-  rect(0, 0, 100)
+  clear();
 
+  background(0,0,75);
+  //the base
+  fill('orange')
+  circle(0,mouseY, 75)
+  //the barrel
+  stroke('orange')
+  rect(0, mouseY-10, 75, 20)
+  stroke('black');
+  fill('black');
+  circle(75, mouseY, 20)
+
+  //projectile
+  stroke('grey');
+  fill('grey');
+  circle(90, cannonballX, 17)
 }
