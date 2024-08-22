554fb5045ab6e43cd050ac5575eb60dcf0d9394d
Sworple <145728862+Sworple@users.noreply.github.com>
2024-08-22 10:02:56 -0800

    shift the p5.js to a different folder, and create a new homepage

--- revamp.2/README.md
+++
@@ -1,2 +0,0 @@
-# sworple.github.io
-it's my site! man are W3 School's CSS and JavaScript courses really useful.
---
+++ wallDefense/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  //Creates the canvas using windowHeight and windowWidth
+  createCanvas(windowWidth, windowHeight);
+  background(0,0,75);
+}
+
+function draw() {
+  rect(0, 0, 100)
+
+}
